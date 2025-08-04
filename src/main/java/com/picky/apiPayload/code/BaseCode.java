0114badba378633f7d8dee06539a8d357c5b2ebc
chaechaen <leece144@gmail.com>
2025-08-05 02:40:19 +0900

    feat: 공통 응답 및 예외 처리를 위한 패키지 추가

---
+++ src/main/java/com/picky/apiPayload/code/BaseCode.java
@@ -0,0 +1,9 @@
+package com.picky.apiPayload.code;
+
+
+public interface BaseCode {
+
+    ReasonDTO getReason();
+
+    ReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/com/picky/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,8 @@
+package com.picky.apiPayload.code;
+
+public interface BaseErrorCode {
+
+    ErrorReasonDTO getReason();
+
+    ErrorReasonDTO getReasonHttpStatus();
+}
