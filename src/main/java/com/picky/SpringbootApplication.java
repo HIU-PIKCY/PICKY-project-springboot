16d6f4844c388e0a6e1a73c005f7e3f4f7585e41
chaechaen <leece144@gmail.com>
2025-08-05 23:06:40 +0900

    feat: BaseEntity 추가

--- src/main/java/com/picky/SpringbootApplication.java
+++ src/main/java/com/picky/SpringbootApplication.java
@@ -1,8 +1,10 @@
-package com.picky.springboot;
+package com.picky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootApplication {
 
