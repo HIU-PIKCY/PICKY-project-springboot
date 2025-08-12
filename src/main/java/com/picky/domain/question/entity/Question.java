df3b2dae1be66cefd1400226001f686cdb43c6f7
chaechaen <leece144@gmail.com>
2025-08-13 03:03:51 +0900

    refactor: 스웨거용 @Schema 어노테이션을 엔티티에서 DTO로 이동

--- src/main/java/com/picky/domain/question/entity/Question.java
+++ src/main/java/com/picky/domain/question/entity/Question.java
@@ -6,7 +6,6 @@ import com.picky.domain.member.entity.Member;
 import com.picky.domain.question.entity.enums.QuestionType;
 import com.picky.domain.questionLike.entity.QuestionLike;
 import com.picky.global.entity.BaseEntity;
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -40,14 +39,14 @@ public class Question extends BaseEntity {
   @JoinColumn(name = "member_id", foreignKey = @ForeignKey(name = "fk_question_member"))
   private Member member;
 
-  @Schema(description = "내용", example = "민음사")
+  @Column(nullable = false)
+  private String title;
+
   @Column(nullable = false, length = 512)
   private String content;
 
-  @Schema(description = "페이지 수", example = "46")
   private Integer pageNum;
 
-  @Schema(description = "AI 생성 여부", example = "false")
   @Builder.Default
   private Boolean isAiGenerated = false;
 
