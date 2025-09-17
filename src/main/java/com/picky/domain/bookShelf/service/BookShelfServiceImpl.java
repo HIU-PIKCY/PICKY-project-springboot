b85a03805f7e134bbc727f953f62146d240d668d
khmandarrin <jeong-ga-eun@jeong-ga-eun-ui-MacBookAir.local>
2025-09-18 03:00:08 +0900

    member 가져오기

--- src/main/java/com/picky/domain/bookShelf/service/BookShelfServiceImpl.java
+++ src/main/java/com/picky/domain/bookShelf/service/BookShelfServiceImpl.java
@@ -18,8 +18,6 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
--- src/main/java/com/picky/domain/bookShelf/web/controller/BookShelfController.java
+++ src/main/java/com/picky/domain/bookShelf/web/controller/BookShelfController.java
@@ -4,7 +4,7 @@ import com.picky.apiPayload.ApiResponse;
 import com.picky.apiPayload.code.status.ErrorStatus;
 import com.picky.apiPayload.code.status.SuccessStatus;
 import com.picky.apiPayload.exception.GeneralException;
-import com.picky.domain.auth.web.CurrentUser;
+import com.picky.domain.auth.CustomerUserDetails;
 import com.picky.domain.bookShelf.entity.BookShelf;
 import com.picky.domain.bookShelf.service.BookShelfService;
 import com.picky.domain.bookShelf.web.dto.BookShelfResponseDTO;
@@ -12,14 +12,12 @@ import com.picky.domain.bookShelf.web.dto.BookShelfResponseDTO.GetBookShelfRespo
 import com.picky.domain.bookShelf.web.dto.BookShelfRequestDTO.GetBookShelfRequestDTO;
 import com.picky.domain.bookShelf.web.dto.BookShelfRequestDTO.PatchBookShelfRequestDTO;
 import com.picky.domain.member.entity.Member;
-import com.picky.domain.member.service.MemberService;
-import com.picky.global.common.PagedMetaDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,24 +32,28 @@ public class BookShelfController {
 
     @Operation(summary = "내 서재 목록 조회 API", description = "내 서재 목록을 조회합니다.")
     @GetMapping()
-    public ApiResponse<GetBookShelfResponseDTO> getBookShelf(@Valid @ModelAttribute GetBookShelfRequestDTO request)
+    public ApiResponse<GetBookShelfResponseDTO> getBookShelf(
+            @AuthenticationPrincipal CustomerUserDetails customerUserDetails,
+            @Valid @ModelAttribute GetBookShelfRequestDTO request )
     {
         Pageable pageable = request.toPageable();
-        Member member = CurrentUser.get();
+        Member currentMember = customerUserDetails.getMember();
 
-        GetBookShelfResponseDTO bookShelf = bookShelfService.getBookShelf(request, member.getId(), pageable);
+        GetBookShelfResponseDTO bookShelf = bookShelfService.getBookShelf(request, currentMember.getId(), pageable);
 
         return ApiResponse.onSuccess(bookShelf);
     }
 
     @Operation(summary = "내 서재 책 삭제 API", description = "내 서재에서 책을 삭제합니다.")
     @DeleteMapping("/{id}")
-    public ApiResponse<SuccessStatus> deleteBookShelf(@PathVariable Long id) {
+    public ApiResponse<SuccessStatus> deleteBookShelf(
+            @AuthenticationPrincipal CustomerUserDetails customerUserDetails,
+            @PathVariable Long id) {
         BookShelf bookShelf = bookShelfService.findById(id);
         if(bookShelf==null) throw new GeneralException(ErrorStatus.BOOKSHELF_NOT_FOUND);
-        Member member = CurrentUser.get();
+        Member currentMember = customerUserDetails.getMember();
 
-        if (!bookShelf.getMember().getId().equals(member.getId())) {
+        if (!bookShelf.getMember().getId().equals(currentMember.getId())) {
             throw new GeneralException(ErrorStatus._FORBIDDEN);
         }
         bookShelfService.deleteBookShelf(bookShelf.getId());
@@ -60,12 +62,14 @@ public class BookShelfController {
 
     @Operation(summary = "내 서재 책 상태 변경 API", description = "내 서재에서 책의 읽기 상태를 변경합니다.")
     @PatchMapping("/{id}")
-    public ApiResponse<BookShelfResponseDTO> patchBookStatus(@PathVariable Long id, @Valid @RequestBody PatchBookShelfRequestDTO request) {
+    public ApiResponse<BookShelfResponseDTO> patchBookStatus(
+            @AuthenticationPrincipal CustomerUserDetails customerUserDetails,
+            @PathVariable Long id, @Valid @RequestBody PatchBookShelfRequestDTO request) {
         BookShelf bookShelf = bookShelfService.findById(id);
         if(bookShelf==null) throw new GeneralException(ErrorStatus.BOOKSHELF_NOT_FOUND);
-        Member member = CurrentUser.get();
+        Member currentMember = customerUserDetails.getMember();
 
-        if (!bookShelf.getMember().getId().equals(member.getId())) {
+        if (!bookShelf.getMember().getId().equals(currentMember.getId())) {
             throw new GeneralException(ErrorStatus._FORBIDDEN);
         }
         BookShelfResponseDTO dto = bookShelfService.patchBookStatus(bookShelf.getId(), request);
