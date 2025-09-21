c2d0bc3fa9cbb4a39626d2261e3d8053116e0a90
yeeunli <lye920@g.hongik.ac.kr>
2025-09-21 23:32:12 +0900

    chore: @RequestBody 어노테이션 수정

--- src/main/java/com/picky/domain/member/web/controller/MemberController.java
+++ src/main/java/com/picky/domain/member/web/controller/MemberController.java
@@ -7,16 +7,12 @@ import com.picky.domain.member.service.MemberService;
 import com.picky.domain.member.web.dto.MemberResponseDTO;
 import com.picky.domain.member.web.dto.PatchMemberRequestDTO;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
