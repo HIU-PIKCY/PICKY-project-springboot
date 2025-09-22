40c568addecfe021fd087ad2ffb728021a0c4315
khmandarrin <jeong-ga-eun@jeong-ga-eun-ui-MacBookAir.local>
2025-09-22 18:42:55 +0900

    질문 답변 기능

--- src/main/java/com/picky/domain/ai/web/controller/AiController.java
+++ src/main/java/com/picky/domain/ai/web/controller/AiController.java
@@ -1,11 +1,13 @@
 package com.picky.domain.ai.web.controller;
 
 import com.picky.domain.ai.service.AiService;
-import com.picky.domain.ai.service.QuestionAiUpdateService;
+import com.picky.domain.ai.web.dto.AiRequestDTO.AiAnswerRequestDTO;
+import com.picky.domain.answer.web.dto.AnswerResponseDTO.AnswerCreateResponseDTO;
 import com.picky.domain.auth.CustomerUserDetails;
-import com.picky.domain.ai.web.dto.AiQuestionRequestDTO;
+import com.picky.domain.ai.web.dto.AiRequestDTO.AiQuestionRequestDTO;
 import com.picky.domain.member.entity.Member;
 import com.picky.domain.question.web.dto.QuestionResponseDTO.QuestionPostResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,8 +23,8 @@ import reactor.core.scheduler.Schedulers;
 public class AiController {
 
     private final AiService aiService;
-    private final QuestionAiUpdateService questionAiUpdateService;
 
+    @Operation(summary = "AI 질문 생성 API", description = "책과 주제에 기반한 질문을 생성합니다.")
     @PostMapping("/generate-question")
     public Mono<ResponseEntity<QuestionPostResponseDTO>> generateQuestion(
             @AuthenticationPrincipal CustomerUserDetails customerUserDetails,
@@ -33,4 +34,17 @@ public class AiController {
         return aiService.generateQuestion(questionRequestDTO, currentMember)
                 .map(ResponseEntity::ok);
     }
+
+    @Operation(summary = "AI 답변 생성 API", description = "질문에 기반한 답변을 생성합니다.")
+    @PostMapping("/generate-answer/{questionId}")
+    public Mono<ResponseEntity<AnswerCreateResponseDTO>> generateAnswer(
+            @AuthenticationPrincipal CustomerUserDetails customerUserDetails,
+            @PathVariable Long questionId
+    ) {
+        Member currentMember = customerUserDetails.getMember();
+
+        return aiService.generateAnswer(questionId, currentMember)
+                .map(ResponseEntity::ok);
+    }
+
 }
